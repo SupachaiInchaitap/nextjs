56937c066558e66c40f7313024049205a6c710a8
SupachaiInchaitap <150765681+SupachaiInchaitap@users.noreply.github.com>
2025-02-06 14:42:17 +0700

    Update Banner.tsx

--- app/components/Banner/Banner.tsx
+++ app/components/Banner/Banner.tsx
@@ -19,10 +19,10 @@ const Banner = () => {
 
                     <div className="text-center mt-5">
                         <button type="button" className='text-15px text-white font-medium bg-blue py-5 px-9 mt-2 leafbutton'>
-                            See our portfolio
+                            66026190
                         </button>
                         <button type="button" className='text-15px ml-4 mt-2 text-blue transition duration-150 ease-in-out hover:text-white hover:bg-blue font-medium py-5 px-16 border border-lightgrey leafbutton'>
-                            More info
+                            66026190
                         </button>
                         
                     </div>
